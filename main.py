ffbe3a0504d746627bbbb4c5a8f38867b05093a1
krayzius-wolf <48599554+krayzius-wolf@users.noreply.github.com>
2020-06-23 12:59:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ if args.set_class_iou is not None:
 # make sure that the cwd() is the location of the python script (so that every path makes sense)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-GT_PATH = os.path.join(os.getcwd(), 'input', 'ground-truth')
-DR_PATH = os.path.join(os.getcwd(), 'input', 'detection-results')
+GT_PATH = '/content/drive/My Drive/ground-truth'
+DR_PATH = '/content/drive/My Drive/detection-results'
 # if there are no images then no animation can be shown
 IMG_PATH = os.path.join(os.getcwd(), 'input', 'images-optional')
 if os.path.exists(IMG_PATH): 
